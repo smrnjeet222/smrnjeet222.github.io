8d653178019825a56cf77a279a59410be9b98380
Simranjeet Singh <smrnjeet222@gmail.com>
2020-07-07 00:19:28 +0530

    preloade

--- js/loader.js
+++ js/loader.js
@@ -1,19 +1,21 @@
 const main = document.querySelector('#main');
 const prt = document.querySelector('#particles-js');
 const btn = document.querySelector('.button');
+const loader = document.getElementById('load');
 
 function init() {
+  
   setTimeout(() => {
-
-    const loader = document.getElementById('load');
+    
     loader.href = '';
-
+    
     main.style.display = 'block';
     btn.style.display = 'inline-block';
 
     setTimeout(() => (prt.style.opacity = 1), 100);
+    setTimeout(() => (main.style.opacity = 1), 100);
     setTimeout(() => (btn.style.opacity = 1), 50);
-  }, 2400);
+  }, 2000);
 }
 
-init();
+window.onload = init();
--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+window.onload = function(){
 // Wrap every letter in a span
 var textWrapper = document.querySelector('.ml9 .letters');
 textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
@@ -5,28 +6,28 @@ textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='let
 var textWrapper2 = document.querySelector('.ml9 .letters2');
 textWrapper2.innerHTML = textWrapper2.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
 
-
-anime.timeline({loop: false })
-.add({
-  targets: '.ml9 .letter',
-  scale: [0, 0],
-  duration: 900,
-  elasticity: 600,
-  delay:2500,
-}).add({
-  targets: '.ml9 .letter',
-  translateY: ["1.1em", 0],
-  translateZ: 0,
-  duration: 1000,
-  elasticity: 500,
-  delay: (el, i) => 50 * (i+1)
-}).add({
-  targets: '.ml9',
-  opacity: 100,
-  duration: 1000,
-  easing: "easeOutExpo",
-  delay: 1000
-});
+  anime.timeline({loop: false })
+  .add({
+    targets: '.ml9 .letter',
+    scale: [0, 0],
+    duration: 900,
+    elasticity: 600,
+    delay:2200,
+  }).add({
+    targets: '.ml9 .letter',
+    translateY: ["1.1em", 0],
+    translateZ: 0,
+    duration: 1000,
+    elasticity: 500,
+    delay: (el, i) => 50 * (i+1)
+  }).add({
+    targets: '.ml9',
+    opacity: 100,
+    duration: 1000,
+    easing: "easeOutExpo",
+    delay: 1000
+  });
+}
 
 
 
