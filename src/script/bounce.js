626074bd4eac47a70c0acd261707411611143032
Simranjeet Singh <smrnjeet222@gmail.com>
2020-09-16 18:23:32 +0530

    home

---
+++ src/script/bounce.js
@@ -0,0 +1,17 @@
+export default function bounce() {
+  const cls = document.querySelectorAll(".blast");
+
+  cls.forEach((cl) => {
+    cl.addEventListener("mouseover", function () {
+      cl.classList.add("animated");
+      cl.classList.add("rubberBand");
+    });
+
+    cl.addEventListener("mouseleave", function () {
+      setTimeout(function () {
+        cl.classList.remove("animated");
+        cl.classList.remove("rubberBand");
+      }, 1000);
+    });
+  });
+}
