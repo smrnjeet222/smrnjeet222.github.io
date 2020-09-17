51096db4568a49543c8f32e4e9bd35ab314eb9a2
Simranjeet Singh <smrnjeet222@gmail.com>
2020-09-17 16:08:42 +0530

    Updates

---
+++ precache-manifest.0f562b9d9d26230f6cc807041933cb27.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1caa1faa247419f139cc6d82f0c02800",
+    "url": "./index.html"
+  },
+  {
+    "revision": "ab937333efbce2a80d27",
+    "url": "./static/css/main.d34c6bcc.chunk.css"
+  },
+  {
+    "revision": "8d88f93911ccc6a6bfba",
+    "url": "./static/js/2.477d4e01.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.477d4e01.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ab937333efbce2a80d27",
+    "url": "./static/js/main.e6e93039.chunk.js"
+  },
+  {
+    "revision": "610a38b7b5a7cb7b2d5e",
+    "url": "./static/js/runtime-main.916853f0.js"
+  }
+]);
